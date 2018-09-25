666eb4ae4952bd8192ded75030c073e1075f7c92
Ephraim Muhia <emuhia@ona.io>
2018-09-25 10:32:05 +0300

    Formatting test classes
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- src/test/java/org/opensrp/common/domain/UserDetailTest.java
+++ src/test/java/org/opensrp/common/domain/UserDetailTest.java
@@ -1,26 +1,27 @@
 package org.opensrp.common.domain;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
+import org.junit.Test;
 
 /**
  * Created by real on 12/07/17.
  */
 public class UserDetailTest {
-    @Test
-    public void testConstructorNGetters() {
-        List<String> rolesList = new ArrayList<>();
-        rolesList.add("TLI");
-        rolesList.add("FD");
-        UserDetail userDetail = new UserDetail("real", rolesList);
-        assertEquals("real", userDetail.userName());
-        assertNotSame("peal", userDetail.userName());
-        assertEquals("TLI", userDetail.roles().get(0));
-        assertNotSame("TLI", userDetail.roles().get(1));
-    }
+	
+	@Test
+	public void testConstructorNGetters() {
+		List<String> rolesList = new ArrayList<>();
+		rolesList.add("TLI");
+		rolesList.add("FD");
+		UserDetail userDetail = new UserDetail("real", rolesList);
+		assertEquals("real", userDetail.userName());
+		assertNotSame("peal", userDetail.userName());
+		assertEquals("TLI", userDetail.roles().get(0));
+		assertNotSame("TLI", userDetail.roles().get(1));
+	}
 }
