eca7002683668f69eed60184fcc20461fc168055
Samuel Githengi <samuel.githengi@gmail.com>
2020-04-28 17:20:33 +0300

    Upgdate user details

--- src/main/java/org/opensrp/common/domain/UserDetail.java
+++ src/main/java/org/opensrp/common/domain/UserDetail.java
@@ -7,13 +7,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class UserDetail implements Serializable {
 	
-	@JsonProperty
+	@JsonProperty("preferred_username")
 	private String userName;
+	
+	@JsonProperty("sub")
+	private String identifier;
 	
 	@JsonProperty
 	private List<String> roles;
 	
+	@JsonProperty("name")
 	private String preferredName;
+	
+	@JsonProperty("family_name")
+	private String familyName;
+	
+	@JsonProperty("given_name")
+	private String givenName;
+	
+	private String email;
+	
+	@JsonProperty("email_verified")
+	private boolean emailVerified;
 	
 	public UserDetail(String userName, List<String> roles) {
 		this.userName = userName;
@@ -27,13 +42,61 @@ public class UserDetail implements Serializable {
 	public List<String> getRoles() {
 		return roles;
 	}
-
+	
 	public String getPreferredName() {
 		return preferredName;
 	}
-
+	
 	public void setPreferredName(String preferredName) {
 		this.preferredName = preferredName;
+	}
+	
+	public String getIdentifier() {
+		return identifier;
+	}
+	
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	
+	public String getFamilyName() {
+		return familyName;
+	}
+	
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	
+	public String getGivenName() {
+		return givenName;
+	}
+	
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public boolean isEmailVerified() {
+		return emailVerified;
+	}
+	
+	public void setEmailVerified(boolean emailVerified) {
+		this.emailVerified = emailVerified;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
 	}
 	
 }
